f0a780f7f968f98c7a3c298ce5917dd2a09f7488
robeerah <rofiat.ahmed@gmail.com>
2022-12-09 02:11:16 -0800

    If esle Task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char xy;
+
+	for (xy = 'a'; xy <= 'z'; xy++)
+	{
+		putchar(xy);
+	}
+	putchar('\n');
+		return (0);
+}
