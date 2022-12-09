a139d184cd224dbe59b166e4a6b8eae93f2cd444
robeerah <rofiat.ahmed@gmail.com>
2022-12-09 03:37:20 -0800

    task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 /**
  * main - prints the alphabet in lowercase followed by a new line, except q and e
- * 
- * return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -16,6 +14,5 @@ int main(void)
 	}
 
 	putchar('\n');
-
 	return (0);
 }
