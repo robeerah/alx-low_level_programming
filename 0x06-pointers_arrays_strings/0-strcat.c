04ee192254cb03170d5acabd153216dec5784a36
robeerah <rofiat.ahmed@gmail.com>
2022-12-22 13:42:14 -0800

    Pointer task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 
 /**
  * _strchar - concatenates the string pointed to by @sec to
- * the end of the string pointed to by @dest
+ * the end of th string pointed to by @dest
  * @dest: String that will be appended
  * @src: String to be concatenated upon
  *
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two string but add inputted number of bytes
- * @dest: String to be appended upon
- * @src: String to be completed at the end of dest
+ * _strncat - concatenate two strings but add inputted number of bytes
+ * @dest: string to be appended upon
+ * @src: string to be completed at the end of dest
  * @n: integer parameter to compare index to
  * Return: returns new concatenated string
  */
@@ -11,12 +11,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
-	
+
 	while (dest[index++])
 		dest_len++;
-	
+
 	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
-	
+
 	return (dest);
 }
