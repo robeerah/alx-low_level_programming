8aec389dc27b9db44ac93d4d3ecab4b08cc9096c
robeerah <rofiat.ahmed@gmail.com>
2022-12-22 09:48:20 -0800

    Pointer task

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN.H
+#define MAIN.H
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ * int prototype()
+ * char prototypes()
+ */
+
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, char size);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *Src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
+#endif /*MAIN.H/
