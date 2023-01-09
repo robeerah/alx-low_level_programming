8ac1e95035168fa9a21854408a7a5dc97238f7e3
robeerah <rofiat.ahmed@gmail.com>
2023-01-09 12:49:35 -0800

    task

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
